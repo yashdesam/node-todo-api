b42e6c8dfdcafd5110db19600991a5b565c040c8
Yaswanth reddy Desam <yaswanth@Yaswanths-MacBook-Pro.local>
2018-02-25 16:33:15 -0500

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,34 @@
+//const mongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // // deleteMany
+  // db.collection('Users').deleteMany({
+  //   name: 'yaswanth',
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // // deleteOne
+  // db.collection('Todos').deleteOne({
+  //   text: 'Walk the dog',
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOne and deteteOne
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('5a93174eeff05162b8706f36'),
+  }).then((result) => {
+    console.log(result);
+  });
+
+  //client.close();
+});
