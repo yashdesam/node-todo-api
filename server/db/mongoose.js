68a64dc1e622a812c776064f959fdaf3ef5db2b0
Yaswanth reddy Desam <yaswanth@Yaswanths-MacBook-Pro.local>
2018-02-26 17:12:11 -0500

    Add DELETE /todo/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 mongoose.promise = global.Promise;
 
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://yashdesam:yashdesam@ds249428.mlab.com:49428/todoapp');
+mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://yashdesam:yashdesam@ds249428.mlab.com:49428/todoapp');
 
 module.exports = { mongoose };
