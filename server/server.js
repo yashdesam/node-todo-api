9ed52dc1de4a3a5e668a94673efea12ed99f47a8
Yaswanth reddy Desam <yaswanth@Yaswanths-MacBook-Pro.local>
2018-02-26 13:40:40 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res)=> {
+app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text,
   });
@@ -21,7 +21,13 @@ app.post('/todos', (req, res)=> {
   res.status(400).send(e);
 });
 
-app.get('/todos');
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos });
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
